5d9119882212c40f16f5e3627337182c9acda733
Aiden Cullo <aidencullo@gmail.com>
2025-09-29 17:24:45 -0400

    Add debug print and fix syntax error

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 	"time"
@@ -8,14 +9,14 @@ import (
 
 func hello(w http.ResponseWriter, r *http.Request) {
 	start := time.Now()
-	
+
 	log.Printf("Request: %s %s %s", r.Method, r.URL.Path, r.Proto)
 	log.Printf("Remote address: %s", r.RemoteAddr)
 	log.Printf("User-Agent: %s", r.UserAgent())
 
 	message := "Hello from the server!"
 	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
-	
+
 	if _, err := w.Write([]byte(message + "\n")); err != nil {
 		log.Printf("Error writing response: %v", err)
 	}
@@ -30,7 +31,4 @@ func main() {
 
 	http.HandleFunc("/", hello)
 
-	if err := http.ListenAndServe(":8080", nil); err != nil {
-		log.Fatalf("Server failed to start: %v", err)
-	}
-}
+push
